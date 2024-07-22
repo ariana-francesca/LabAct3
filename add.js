594cb0a2d60f96533f652984bbc8468b0e410400
Ariana <anpunsalang@student.hau.edu.ph>
2024-07-22 14:19:09 +0800

    Create add.js

---
+++ add.js
@@ -0,0 +1,8 @@
+const add = require('./app.js');
+
+const num1 = 15;
+const num2 = 7;
+
+const result = add(num1, num2);
+
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + result + '');
